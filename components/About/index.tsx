a6895695bcbf87a354b8f82474b74b9b6e366bc0
Lucas Souza <lucasvsouza28@gmail.com>
2022-12-09 19:14:25 -0300

    fix: 'about' section animations

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -21,29 +21,29 @@ const About = (props: Props) => {
                 src="https://cataas.com/cat"
                 className='rounded-full w-60 h-60 object-cover md:rounded-lg md:h-[300px] md:w-[300px]'
                 initial={{
-                    top: -300,
                     opacity: 0,
+                    translateY: -300
                 }}
                 animate={{
-                    top: 0,
                     opacity: 1,
+                    translateY: 0
                 }}
                 transition={{
-                    duration: 1
+                    duration: 1,
                 }}
             />
             <motion.p
                 className='text-justify px-8 text-xl'
                 initial={{
-                    bottom: -300,
+                    translateY: 300,
                     opacity: 0,
                 }}
                 animate={{
-                    bottom: 0,
+                    translateY: 0,
                     opacity: 1,
                 }}
                 transition={{
-                    duration: 1
+                    duration: 1,
                 }}
             >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit expedita eaque sit explicabo, doloribus officiis? Saepe quia illum qui aut vel cumque quis beatae id, fuga reprehenderit aliquam vero voluptates.
