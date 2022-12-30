0315b36f02a990fdb46224c76d5eb1ccbe78b078
Lucas Souza <lucasvsouza28@gmail.com>
2022-12-29 22:40:43 -0300

    wip: listing github projects

---
+++ components/Projects/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { UserRepo } from '../../@types';
+import SectionHeader from '../SectionHeader'
+
+export type Project = {
+  title: string;
+  description: string;
+};
+
+type ProjectsProps = {
+  repos: UserRepo[],
+}
+
+const Projects = ({
+  repos,
+}: ProjectsProps) => {
+  return (
+    <>
+        <SectionHeader
+            title='Projects'
+        />
+
+        <div
+          className='grid grid-rows-3 grid-flow-col gap-2
+          overflow-x-scroll
+          scroll-smooth scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100
+          snap snap-mandatory snap-x'
+        >
+          {repos.map((item, i) => (
+            <div
+              key={i}
+              className='
+              snap-center
+              p-5
+              relative
+              before:absolute before:rounded-lg before:-z-10 before:backdrop-blur-[5px] before:w-full before:h-full before:inset-0 before:border before:border-white/10 before:bg-white/10'
+            >
+              <p>
+                {item.name}
+              </p>
+              <p>
+                {item.description}
+              </p>
+            </div>
+          ))}
+        </div>        
+    </>
+  )
+}
+
+export default Projects
---
+++ services/github.ts
@@ -0,0 +1,11 @@
+const getRepositories = async () => {
+    const response = await fetch("https://api.github.com/users/lucasvsouza28/repos")
+
+    if (!response.ok) throw new Error(`[GET_USER_REPOSITORIES] ${response.statusText}`);
+
+    const data = await response.json() as UserRepo[];
+
+    return data;
+}
+
+export default getRepositories;
